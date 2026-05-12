cfcac966927220786289d9d7646b9434bcb9fd30
AB <aby11wibowo@gmail.com>
2026-05-12 14:03:46 +0700

    Add files via upload

---
+++ bd_braille_print.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2026 at 08:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bd_braille_print`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+-- Error reading structure for table bd_braille_print.buku: #1932 - Table &#039;bd_braille_print.buku&#039; doesn&#039;t exist in engine
+-- Error reading data for table bd_braille_print.buku: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bd_braille_print`.`buku`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pesanan`
+--
+-- Error reading structure for table bd_braille_print.detail_pesanan: #1932 - Table &#039;bd_braille_print.detail_pesanan&#039; doesn&#039;t exist in engine
+-- Error reading data for table bd_braille_print.detail_pesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bd_braille_print`.`detail_pesanan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorit`
+--
+-- Error reading structure for table bd_braille_print.favorit: #1932 - Table &#039;bd_braille_print.favorit&#039; doesn&#039;t exist in engine
+-- Error reading data for table bd_braille_print.favorit: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bd_braille_print`.`favorit`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+-- Error reading structure for table bd_braille_print.pesanan: #1932 - Table &#039;bd_braille_print.pesanan&#039; doesn&#039;t exist in engine
+-- Error reading data for table bd_braille_print.pesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bd_braille_print`.`pesanan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table bd_braille_print.users: #1932 - Table &#039;bd_braille_print.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table bd_braille_print.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bd_braille_print`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
